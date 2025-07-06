282b51c96ce6fc495aa249dc863a4fa47a76f8b6
Børre A. Opedal Lunde <borre97@live.no>
2025-07-06 23:41:28 +0200

    feat: implement `DimensionWithTolerance` class and add tests
    - implement `DimensionWithTolerance` class with factory method and getters
    - add unit tests for `DimensionWithTolerance` in `DimensionWithTolerance.test.ts`

--- src/DimensionWithTolerance.ts
+++ src/DimensionWithTolerance.ts
@@ -1,3 +1,35 @@
+import {Dimension} from "./Dimension";
+import {Tolerance} from "./Tolerance";
+
+/**
+ * Represents a dimension with a tolerance associated with it.
+ */
 export class DimensionWithTolerance {
-    // To be implemented.
+
+    /**
+     * Private constructor to enforce the use of the factory method.
+     */
+    private constructor(private readonly dimension: Dimension,
+                        private readonly tolerance: Tolerance) {
+        // The fields are assigned via parameter properties.
+    }
+
+    /**
+     * Static factory method for constructing a DimensionWithTolerance from a
+     * dimension and tolerance.
+     *
+     * @param dimension the dimension.
+     * @param tolerance the associated tolerance.
+     */
+    static fromDimensionAndTolerance(dimension: Dimension, tolerance: Tolerance): DimensionWithTolerance {
+        return new DimensionWithTolerance(dimension, tolerance);
+    }
+
+    getDimension(): Dimension {
+        return this.dimension;
+    }
+
+    getTolerance(): Tolerance {
+        return this.tolerance;
+    }
 }
